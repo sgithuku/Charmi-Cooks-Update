f6bbbdf300a0ba562642c737dc2a99541025bdef
Shaun <shaungithuku@gmail.com>
2020-04-19 21:03:13 +0100

    Butter chicken and no sitemap

---
+++ content/posts/2020-13-04---Butter-chicken.md
@@ -0,0 +1,49 @@
+---
+title: Butter Chicken
+date: "2020-13-04T22:00:32.169Z"
+template: "post"
+draft: false
+slug: "/posts/butter-chicken/"
+category: "indian"
+tags:
+  - "Recipe"
+  - "Indian"
+description: "Butter chicken made from scratch with all the fixings"
+---
+
+## Butter chicken (Chicken Makhani)
+
+## Ingredients:
+
+- Chicken thighs (boneless 500g)
+- Salted Butter
+- 1 large onion (finely chopped)
+- 3 large cloves garlic
+- Ginger
+- 2 finger chillies
+- Red chilli powder
+- Garam masala
+- Coriander powder
+- Cumin powder
+- Salt
+- Double cream
+- Kasthuri Methi
+- Tomato puree
+- Cardamom (5)
+- Cloves (5)
+- Bay leaf (2)
+- Cinnamon bark (1)
+- Honey 2 teaspoons
+- Coriander
+
+### Chicken marinade ingredients
+
+- Low fat natural yogurt
+  - Ginger
+  - Garlic
+  - Green chillies
+  - Turmeric
+  - Coriander powder
+  - Cumin powder
+  - Garam masala
+  - Red chilli powder
--- gatsby-config.js
+++ gatsby-config.js
@@ -134,38 +134,38 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: "gatsby-plugin-sitemap",
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                url
-              }
-            }
-            allSitePage(
-              filter: {
-                path: { regex: "/^(?!/404/|/404.html|/dev-404-page/)/" }
-              }
-            ) {
-              edges {
-                node {
-                  path
-                }
-              }
-            }
-          }
-        `,
-        output: "/sitemap.xml",
-        serialize: ({ site, allSitePage }) =>
-          allSitePage.edges.map((edge) => ({
-            url: site.siteMetadata.url + edge.node.path,
-            changefreq: "daily",
-            priority: 0.7,
-          })),
-      },
-    },
+    // {
+    //   resolve: "gatsby-plugin-sitemap",
+    //   options: {
+    //     query: `
+    //       {
+    //         site {
+    //           siteMetadata {
+    //             url
+    //           }
+    //         }
+    //         allSitePage(
+    //           filter: {
+    //             path: { regex: "/^(?!/404/|/404.html|/dev-404-page/)/" }
+    //           }
+    //         ) {
+    //           edges {
+    //             node {
+    //               path
+    //             }
+    //           }
+    //         }
+    //       }
+    //     `,
+    //     output: "/sitemap.xml",
+    //     serialize: ({ site, allSitePage }) =>
+    //       allSitePage.edges.map((edge) => ({
+    //         url: site.siteMetadata.url + edge.node.path,
+    //         changefreq: "daily",
+    //         priority: 0.7,
+    //       })),
+    //   },
+    // },
     {
       resolve: "gatsby-plugin-manifest",
       options: {
