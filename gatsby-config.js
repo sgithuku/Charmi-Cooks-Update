6bd5da1392ad63e7124652d12c37ad55a0b1b2d5
Shaun <shaungithuku@gmail.com>
2020-04-20 20:40:03 +0100

    Mo sitemaps

--- gatsby-config.js
+++ gatsby-config.js
@@ -135,38 +135,39 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: "gatsby-plugin-sitemap",
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                url
-              }
-            }
-            allSitePage(
-              filter: {
-                path: { regex: "/^(?!/404/|/404.html|/dev-404-page/)/" }
-              }
-            ) {
-              edges {
-                node {
-                  path
-                }
-              }
-            }
-          }
-        `,
-        output: "/sitemap.xml",
-        serialize: ({ site, allSitePage }) =>
-          allSitePage.edges.map((edge) => ({
-            url: site.siteMetadata.url + edge.node.path,
-            changefreq: "daily",
-            priority: 0.7,
-          })),
-      },
-    },
+    "gatsby-plugin-sitemap",
+    // {
+    //   resolve: "gatsby-plugin-sitemap",
+    //   options: {
+    //     query: `
+    //       {
+    //         site {
+    //           siteMetadata {
+    //             siteUrl
+    //           }
+    //         }
+    //         allSitePage(
+    //           filter: {
+    //             path: { regex: "/^(?!/404/|/404.html|/dev-404-page/)/" }
+    //           }
+    //         ) {
+    //           edges {
+    //             node {
+    //               path
+    //             }
+    //           }
+    //         }
+    //       }
+    //     `,
+    //     output: "/sitemap.xml",
+    //     serialize: ({ site, allSitePage }) =>
+    //       allSitePage.edges.map((edge) => ({
+    //         url: site.siteMetadata.url + edge.node.path,
+    //         changefreq: "daily",
+    //         priority: 0.7,
+    //       })),
+    //   },
+    // },
     {
       resolve: "gatsby-plugin-manifest",
       options: {
